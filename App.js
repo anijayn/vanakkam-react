6ceb494d9d1351e2a7ae193a8ec1ced06f3577ba
anijayn <anirudhjayan33@gmail.com>
2025-01-15 19:45:41 +0530

    Episode 1 Done and Dusted 💪

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "HELLOOOOO"),React.createElement("h2", {}, "BYEEEE")]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "HELLOOOOO"),React.createElement("h2", {}, "BYEEEE")]
+  )]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
